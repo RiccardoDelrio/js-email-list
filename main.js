03536a9f36d90258d7688dd3c1b81d8cd467bbb3
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-27 16:03:19 +0100

    implement email generation on button click and update dom elements

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 /* const containerEl = document.querySelector(".container")
  */
 const emailEl = document.querySelector(".email-list")
-for (let i = 0; i < 10; i++) {
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response => response.json())
-        .then(data => {
+const buttonEl = document.querySelector(".generator")
+// for (let i = 0; i < 10; i++) {
+//     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+//         .then(response => response.json())
+//         .then(data => {
 
-            const email = data.response
-            emailEl.innerHTML += `<li class="list-group-item">${email}</li>`
+//             const email = data.response
+//             emailEl.innerHTML += `<li class="list-group-item">${email}</li>`
 
 
-        })
-}
+//         })
+// }
 
+buttonEl.addEventListener("click", () => {
+    emailEl.innerHTML = ""
+    for (let i = 0; i < 10; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+
+            .then(response => response.json())
+            .then(data => {
+                const email = data.response
+                emailEl.innerHTML += `<li class="list-group-item">${email}</li>`
+            })
+    }
+})
