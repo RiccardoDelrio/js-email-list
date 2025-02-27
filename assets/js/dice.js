bb02637beac10c6730c0f1629bf08f2e32fe7f03
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-27 18:11:44 +0100

    refactor dice rolling logic to trigger on button click and update result display

--- assets/js/dice.js
+++ assets/js/dice.js
@@ -3,20 +3,24 @@ const computerEl = document.querySelector('.computer_dice');
 const btnEl = document.querySelector('.btn');
 const resultEl = document.querySelector('.winner_message');
 
-fetch("https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=6&items=2")
-    .then(response => response.json())
-    .then(data => {
-        const [player, computer] = data.response;
-        console.log(player, computer);
 
-        playerEl.innerHTML = `<img src="./assets/img/${player}.svg" alt="">`
-        computerEl.innerHTML = `<img src="./assets/img/${computer}.svg" alt="">`
-        if (player > computer) {
-            resultEl.innerHTML = 'The winner is: Player!'
-        } else if (player === computer) {
-            resultEl.innerHTML = 'pareggio'
+btnEl.addEventListener('click', () => {
+    fetch("https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=6&items=2")
+        .then(response => response.json())
+        .then(data => {
+            const [player, computer] = data.response;
+            console.log(player, computer);
 
-        } else {
-            resultEl.innerHTML = 'The winner is: Computer!'
-        }
-    })
+            playerEl.innerHTML = `<img src="./assets/img/${player}.svg" alt="">`
+            computerEl.innerHTML = `<img src="./assets/img/${computer}.svg" alt="">`
+            if (player > computer) {
+                resultEl.innerHTML = 'The winner is: Player!'
+            } else if (player === computer) {
+                resultEl.innerHTML = 'pareggio'
+
+            } else {
+                resultEl.innerHTML = 'The winner is: Computer!'
+            }
+        })
+
+})
