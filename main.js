6c8d14faf7d7a05abdd83c8d6025072ca7e970a0
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-27 15:52:40 +0100

    add email generator page and update navigation

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const containerEl = document.querySelector(".container")
-
+/* const containerEl = document.querySelector(".container")
+ */
+const emailEl = document.querySelector(".email-list")
 for (let i = 0; i < 10; i++) {
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(response => response.json())
         .then(data => {
 
             const email = data.response
-            containerEl.innerHTML += `<div>${email}</div>`
+            emailEl.innerHTML += `<li class="list-group-item">${email}</li>`
 
 
         })
