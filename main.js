f1de185608e3c30c00099987bbf9c84fa0bbc9cc
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-27 15:28:15 +0100

    fetch and display multiple random emails in the container

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const containerEl = document.querySelector(".container")
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => response.json())
-    .then(data => {
-        const email = data.response
-        containerEl.innerHTML = email
-    })
+
+for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => response.json())
+        .then(data => {
+
+            const email = data.response
+            containerEl.innerHTML += `<div>${email}</div>`
+
+
+        })
+}
+
